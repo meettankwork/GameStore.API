c127e7b89888573c3de185e371a89dbeceb16321
meettankwork <meettankwork@gmail.com>
2026-04-25 14:09:32 +0530

    made changes

--- GameStore.API/GameStore.API/obj/Debug/net10.0/GameStore.API.AssemblyInfo.cs
+++ GameStore.API/GameStore.API/obj/Debug/net10.0/GameStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0460c25a09cc67cb7d96c0c56c337af6dd8124")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a17dae1f531eaa3de087ebb7dfc71ca05e5200a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
